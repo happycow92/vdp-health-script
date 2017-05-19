1d30a14a0b1fa29e4cf534a4cf19f11b5f5ca1e6
Suhas G <virtuallypeculiar@gmail.com>
2017-05-19 15:35:32 +0530

    Add files via upload

---
+++ health-script-mail.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter Your Email: " TO
+sed -r "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g" /home/admin/health-check.txt > /home/admin/health-check-1.txt
+FROM=admin@$(hostname)
+TO=$TO
+BODY_FILE=/home/admin/health-check-1.txt
+
+(cat - $BODY_FILE)<<HEADERS_END | /usr/sbin/sendmail -f $FROM -t $TO
+Subject: VDP System Status
+To: $TO
+HEADERS_END
+
+printf "\nMail Sent. All Done!\n\n"
+rm /home/admin/health-check.txt
+rm /home/admin/health-check-1.txt
